26f31d843d6456563b3721f123d99e5cf80bac75
cissavalim <cissamadalenarv@gmail.com>
2021-10-27 20:09:46 -0300

    update calculadora.js

--- progr-imp/calculadora.js
+++ progr-imp/calculadora.js
@@ -15,11 +15,11 @@ function divisao(a, b) {
 }
 
 console.log("-------------- Teste de Operações / Calculadora --------------");
-console.log("Adicao: " + adicao(10, 5));
-console.log("Subtracao: " + subtracao(12, 5));
-console.log("Multiplicacao: " + multiplicacao(2, 5));
-console.log("Divisao: " + divisao(50, 5));
-console.log("Divisao com 0: " + divisao(50, 0));
+console.log("Adicao entre 10 e 5: " + adicao(10, 5));
+console.log("Subtracao entre 12 e 5: " + subtracao(12, 5));
+console.log("Multiplicacao entre 2 e 5: " + multiplicacao(2, 5));
+console.log("Divisao entre 50 e 5: " + divisao(50, 5));
+console.log("Divisao entre 50 e 0: " + divisao(50, 0));
 
 function quadradoDoNumero(a) {
   return a ^ 2;
@@ -28,11 +28,14 @@ function quadradoDoNumero(a) {
 function mediaDeTresNumeros(a, b, c) {
   return divisao(adicao(adicao(a, b), c), 3);
 }
+console.log("Média: " + mediaDeTresNumeros(1, 2, 3));
 
 function calculaPorcentagem(a, b) {
   return divisao(multiplicacao(b, 100), a);
 }
+console.log("Calcular porcentagem: " + calculaPorcentagem(100, 15));
 
 function geradorDePorcentagem(a, b) {
   return divisao(multiplicacao(a, 100), b);
 }
+console.log("Gerador de porcentagem: " + geradorDePorcentagem(2, 200));
