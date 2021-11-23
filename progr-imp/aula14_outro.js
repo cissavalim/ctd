e365a8c298fd9afdd404f54501a72ea9255d8271
cissavalim <cissamadalenarv@gmail.com>
2021-11-23 20:37:13 -0300

    adicionada funcao soma

--- progr-imp/aula14_outro.js
+++ progr-imp/aula14_outro.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const soma = require("./aula14");
 
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
 app.listen(3000);
+
+console.log(soma(1, 2));
