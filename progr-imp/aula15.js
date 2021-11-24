46eaf2cb903fb20b9870dcc1caa087e322b32ec9
cissavalim <cissamadalenarv@gmail.com>
2021-11-24 20:25:12 -0300

    aula15

---
+++ progr-imp/aula15.js
@@ -0,0 +1,28 @@
+let dados = require("./arquivo");
+let listaUsuarios = [];
+
+function Conta(numeroConta, tipoConta, saldo, titular) {
+  this.numeroConta = numeroConta;
+  this.tipoConta = tipoConta;
+  this.saldo = saldo;
+  this.titular = titular;
+}
+
+for (let i = 0; i < dados.length; i++) {
+  listaUsuarios.push(new Conta(dados[i].numero, dados[i].tipo, dados[i].saldo, dados[i].titular));
+}
+
+let banco = {
+  clientes: listaUsuarios,
+  consultarCliente: function(titular) {
+    for (let i = 0; i < this.clientes.length; i++) {
+      if (this.clientes[i].titular == titular) {
+        return this.clientes[i];
+      } else {
+        return "Não encontrado";
+      }
+    }
+  },
+};
+
+console.log(banco.consultarCliente("Abigael Natte"));
